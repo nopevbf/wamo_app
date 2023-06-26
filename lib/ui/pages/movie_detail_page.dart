508bdc5da37c87be003e498e047445bff77e6e3a
nopevbf <furimanjay@gmail.com>
2023-06-26 14:39:07 +0700

    detail_movie

---
+++ lib/ui/pages/movie_detail_page.dart
@@ -0,0 +1,14 @@
+part of 'pages.dart';
+
+class MovieDetailPage extends StatelessWidget{
+  final Movies movies;
+
+  const MovieDetailPage(this.movies, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
